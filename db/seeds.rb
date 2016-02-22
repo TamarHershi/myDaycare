7d4e9fa2f0da537efb2741ddbacfe9973d6b059f
Tammy <misstamar@gmail.com>
2016-02-22 12:18:32 -0800

    created seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+seed_users = [
+  {name: "Lucy", last_name: "Moulan", email: "Lucy@gmail.com"},
+  {name: "Dora", last_name: "Moulan", email: "Dora@gmail.com"}
+]
+seed_users.each do |teacher|
+  User.create(teacher)
+end
+
+seed_children = [
+  {name: "Michelle", last_name: "Mor", gender: "girl", age: 6, mom_id: 1},
+  {name: "Jone", last_name: "Andy", gender: "boy", age: 2, mom_id: 2},
+  {name: "Kaitlin", last_name: "Dowl", gender: "girl", age: 7, mom_id: 3},
+  {name: "Lena",last_name: "Mor",  gender: "girl", age: 4, mom_id: 4}
+]
+
+seed_children.each do |child|
+  Child.create(child)
+end
