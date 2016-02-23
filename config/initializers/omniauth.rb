25ccdb600e6bdbd820caeac3690032c7f50155f9
Tammy <misstamar@gmail.com>
2016-02-23 15:54:11 -0800

    fixed the bug, now I have new ones

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_SECRET"],
-  scope: 'profile', image_aspect_ratio: 'square', image_size: 48, access_type: 'online', name: 'google'
+  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_SECRET'],
+  :name => "google",
+  :access_type => "online",
+  :client_options => { :ssl => { :verify => false } }
 end
