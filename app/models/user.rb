61fd8dc5a3ed0eeaf24bdc768e54ec366a146e60
Tammy <misstamar@gmail.com>
2016-02-23 15:11:13 -0800

    did it. Yesss, can now use google amniouth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,24 @@
 class User < ActiveRecord::Base
   has_many :children, through: :room
   belongs_to :room
+
+  def self.find_or_create_from_omniauth(auth_hash)
+   user = self.find_by(uid: auth_hash["uid"])
+   if !user.nil?
+     # User found continue on with your life
+     return user
+   else
+     # Create a new user
+     user = User.new
+     user.uid        = auth_hash["uid"]
+     user.provider   = auth_hash["provider"]
+     user.name   = auth_hash["info"]["name"]
+     user.email      = auth_hash["info"]["email"]
+     if user.save
+       return user
+     else
+       return nil
+     end
+   end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#index'
-  get "/auth/google_oauth2/callback", to: "sessions#create"
+  # get "/auth/google_oauth2/callback", to: "sessions#create"
+  get "/auth/:provider/callback", to: "sessions#create"
   post '/users' => 'users#welcome', as: :welcome
   resources :users do
     resources :children do
