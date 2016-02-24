1cf27984e31c61476ed77c7ac5a6626101a885e6
Tammy <misstamar@gmail.com>
2016-02-24 10:50:45 -0800

    looks good. we have current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :current_user
+  before_action :require_login
 
 
   def current_user
@@ -9,5 +11,11 @@ class ApplicationController < ActionController::Base
    rescue ActiveRecord::RecordNotFound
   end
 
+  def require_login
+    unless current_user
+      flash[:error] = "Please log in"
+      redirect_to root_path
+    end
+  end
 
 end
