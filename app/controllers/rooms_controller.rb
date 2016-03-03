71e540166a9e1bb55669bdf2edc0953d0951c23e
Tammy <misstamar@gmail.com>
2016-03-03 15:38:46 -0800

    now the teacher has to create new form every day

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,13 +1,14 @@
 class RoomsController < ApplicationController
 
     def show
-      @room = Room.find(params[:id])
-      @children = @room.children
-      Child.not_attend(@children)
-      @user = @current_user
+        @room = Room.find(params[:id])
+        @children = @room.children
+        Child.not_attend(@children)
+        @user = @current_user
+
     end
 
-    def index
-    end
+  def index
+  end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,22 @@ class UsersController < ApplicationController
   end
 
   def my_class
-    @user = @current_user
-    @children = @user.children
+    if check_new_day
+      @user = @current_user
+      @children = @user.children
+    else
+      redirect_to root_path, notice: "Please push the 'New Day' button"
+      # add flash note
+    end
+  end
+
+  def check_new_day
+    info = @current_user.children.last.infos.last
+    if info.created_at.strftime('%D')!= DateTime.now.strftime('%D')
+      return false
+    else
+      return true
+    end
   end
 
   def attendees
