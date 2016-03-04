582e28e8e6b02b0f647271b1e57feda2c989f49f
Tammy <misstamar@gmail.com>
2016-03-04 09:45:50 -0800

    got rspec to work

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -2,4 +2,15 @@ require 'rails_helper'
 
 RSpec.describe ApplicationController, type: :controller do
 
+  describe "current user" do
+    before :each do
+      request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:google]
+      user = User.find_or_create_from_omniauth(OmniAuth.config.mock_auth[:google])
+      session[:user_id] = user.id
+    end
+    it "sets @current_user to logged in user" do
+
+      expect(assigns(:current_user).id).to eq(session[:user_id])
+    end
+  end
 end
