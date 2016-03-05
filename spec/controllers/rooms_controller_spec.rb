2b18046a7edfec1fe94d076fb2678897fc561dec
Tammy <misstamar@gmail.com>
2016-03-04 16:40:59 -0800

    get edir test in info

--- spec/controllers/rooms_controller_spec.rb
+++ spec/controllers/rooms_controller_spec.rb
@@ -3,3 +3,90 @@ require 'rails_helper'
 RSpec.describe RoomsController, type: :controller do
 
 end
+
+# require 'rails_helper'
+#
+#   RSpec.describe InfosController, type: :controller do
+#     let (:info) { create(:info) }
+#     let (:child) { create(:child) }
+#     let(:user) { User.find_or_create_from_omniauth(OmniAuth.config.mock_auth[:google]) }
+#
+#     before :each do
+#       session[:user_id] = user.id
+#     end
+#
+#     describe "PATCH 'update'" do
+#       let (:info_params) {
+#         { child_id: child.id,
+#           diaper_m: "dry",
+#           breakfast: "none"
+#         }
+#       }
+#       let (:info_bad_params) {
+#         { child_id: nil,
+#           name: "",
+#           last_name: "",
+#           email: "example@gmail.com"
+#         }
+#       }
+#       context "it has information" do
+#         it "redirect to the my_class page" do
+#           patch :update, :info => info_params , :user_id => user.id, :child_id => child.id, :id => info.id
+#           expect(subject).to redirect_to my_class_path(user.id)
+#         end
+#       end
+#     end
+#
+#     describe "GET 'edit'" do
+#       it "renders edit page" do
+#         get :edit, :user_id => user.id, :child_id => child.id, :id => info.id
+#         expect(subject).to render_template :edit
+#       end
+#     end
+#
+#     describe "POST 'new_forms'" do
+#       let(:first_child) {
+#       {  name: "first",
+#         last_name: "first",
+#         email: "first@gmail.com",
+#         attend: true,
+#         room_id: 5 }
+#       }
+#       let(:second_child) {
+#       { name: "first",
+#         last_name: "first",
+#         attend: true,
+#         email: "first@gmail.com",
+#         room_id: 5 }
+#       }
+#       let(:new_info) {
+#       { diaper_m: "wet",
+#         child_id: seconde_child.id,
+#         created_at: "2016-03-03T16:29:30-08:00 ((2457453j,1770s,655923000n),-28800s,2299161j)"}
+#       }
+#       let(:room) {
+#         {id: 5}
+#       }
+#       let(:new_user) {
+#       { email: "user@gmail.com",
+#         name: "user",
+#         provider: "google",
+#         room_id: 5 }
+#       }
+#
+#       context "When it is a new day" do
+#         it "Makes all children be as not attend" do
+#           post :new_forms
+#           expect(first_child.attend).to equal false
+#         end
+#
+#         it "redirect to welcome path with the right notice"
+#         it "created new empty forms for all the children"
+#       end
+#       context "When it is the same day" do
+#         it "redirect to welcome path with right notice"
+#         it "will not create new forms"
+#       end
+#     end
+#
+#   end
