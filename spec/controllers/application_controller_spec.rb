26aceb9b18b3dc0ea42c42adbce08e2c9e3ddc4b
Tammy <misstamar@gmail.com>
2016-03-08 10:34:34 -0800

    I have 3 faiulers in tests. will take care of them later

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
+require 'pry'
+RSpec.describe ApplicationController, type: :controller do
 
-# RSpec.describe ApplicationController, type: :controller do
-#
-#   describe "current user" do
-#     before :each do
-#       request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:google]
-#       user = User.find_or_create_from_omniauth(OmniAuth.config.mock_auth[:google])
-#       session[:user_id] = user.id
-#     end
-#     it "sets @current_user to logged in user" do
-#       expect(assigns(:current_user).id).to eq(session[:user_id])
-#     end
-#   end
-# end
+ let(:user) { User.find_or_create_from_omniauth(OmniAuth.config.mock_auth[:google]) }
+
+  before :each do
+    session[:user_id] = user.id
+  end
+  describe "current user" do
+    it "sets @current_user to logged in user" do
+      expect(assigns(:current_user).id).to eq(session[:user_id])
+    end
+  end
+end
