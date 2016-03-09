a60cc73223c5451023d92845ffdf25e4f82ee3fb
Tammy <misstamar@gmail.com>
2016-03-09 15:25:55 -0800

    fixed some bugs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,21 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   helper_method :current_user
+  helper_method :parent
   before_action :require_login
   before_action :current_user
+  before_action :parent
 
 
   def current_user
    @current_user ||= User.find(session[:user_id]) if session[:user_id]
    rescue ActiveRecord::RecordNotFound
+  end
+
+  def parent
+    if @current_user.user_type == "p"
+      @parent = Parent.find_by(email: @current_user.email)
+    end
   end
 
   def require_login
